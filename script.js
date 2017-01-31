81f9ecdfaeb9dd7c54f2054ecedb7b6fefda524b
laura devere <lauradevh@gmail.com>
2017-01-31 00:38:47 -0500

    adding balance calculation

--- script.js
+++ script.js
@@ -11,11 +11,16 @@ var UIController = (function() {
 		addIncDiv: 'add_income',
 		expValue: 'exp-value',
 		expComment: 'exp-comment',
+		incValue: 'inc-value',
+		incComment: 'inc-comment',
 		expForm: 'exp-form',
 		expBtn: 'add-exp-btn',
+		incBtn: 'add-inc-btn',
 		allTransacList: '.transactions-list',
 		expCategoryList:'.categories',
-		totalExpLabel: 'total-exp'
+		totalExpLabel: 'total-exp',
+		totalIncLabel: 'total-inc',
+		balanceLabel: '.balance-label'
 	};
 
 	var navBar = function(el) {
@@ -69,7 +74,8 @@ var UIController = (function() {
 				element = DOMStrings.allTransacList;
 				html = '<li id="exp-%id%"><i class="ion-ios-telephone-outline"></i><div><span class="expense-category">%description%</span><span class="expense-amount">- %value%</span></div></li>'
 			} else if (type === 'inc') {
-				console.log(obj);
+				element = DOMStrings.allTransacList;
+				html = '<li id="inc-%id%"><i class="im im-banknote"></i><div><span class="income-category">%description%</span><span class="income-amount">+ %value%</span></div></li>'
 			}
 			//Replace the place holder with new text
 			newHtml = html.replace('%id%', obj.id);
@@ -90,40 +96,54 @@ var UIController = (function() {
 			return el;
 		},
 
-		displayBudget: function(obj) {
-			console.log(obj);
+		displayBudget: function(obj) {			
+			document.getElementById(DOMStrings.totalIncLabel).textContent = obj.totalInc;
 			document.getElementById(DOMStrings.totalExpLabel).textContent = obj.totalExp;
+			
+			document.querySelector(DOMStrings.balanceLabel).textContent = obj.balance;
 		},
 
-		clearFields: function() {
+		clearFields: function(type) {
 			var fields, fieldsArr, category;
 
 			category = document.querySelector('.select-category');
 
-			fields = [document.getElementById(DOMStrings.expValue), document.getElementById(DOMStrings.expComment)];
-
+			if (type === 'exp') {
+				fields = [document.getElementById(DOMStrings.expValue), document.getElementById(DOMStrings.expComment)];
+			} else {
+				fields = [document.getElementById(DOMStrings.incValue), document.getElementById(DOMStrings.incComment)];
+			}
+			
 			fieldsArr = Array.prototype.slice.call(fields);
 
 			fieldsArr.forEach(function(curr, index, array) {
 				curr.value = "";
 			});
 
-			category.classList.remove('backgr-red');
-			category.classList.remove('select-category');
+			category.classList.remove('backgr-red','select-category');
 
 			fieldsArr[0].focus();
 		},
 
-		getInputValue: function() {
+		getInputValue: function(type) {
 			var category;
 
 			category = document.querySelector('.select-category').id;
 
-			return {
+			if (type === 'exp'){
+				return {
 				value: parseFloat(document.getElementById(DOMStrings.expValue).value),
 				comment: document.getElementById(DOMStrings.expComment).value,
 				category: category
-			};
+				};
+			} else {
+				return {
+					value: parseFloat(document.getElementById(DOMStrings.incValue).value),
+					comment: document.getElementById(DOMStrings.incComment).value,
+					category: category
+				};
+			}
+			
 		},
 
 		getNavBar: function(el) {
@@ -137,9 +157,18 @@ var UIController = (function() {
 
 })();
 
+//////////////////////////////////////////BUDGET CONTROLLER//////////////////////////////////////////
+
 var budgetController = (function() {
 
 	var Expense = function(id, value, comment, category) {
+		this.id = id;
+		this.value = value;
+		this.comment = comment;
+		this.category = category;
+	};
+
+	var Income = function(id, value, comment, category) {
 		this.id = id;
 		this.value = value;
 		this.comment = comment;
@@ -154,7 +183,8 @@ var budgetController = (function() {
 		total: {
 			exp: 0,
 			inc: 0
-		}
+		},
+		balance: 0
 	}
 
 	calculateTotals = function(type) {
@@ -168,11 +198,17 @@ var budgetController = (function() {
 	return {
 		calculateBudget: function() {
 			calculateTotals('exp');
+			calculateTotals('inc');
+
+			data.balance = data.total.inc - data.total.exp;
+			console.log(data.balance);
 		},
 
 		getBudget: function() {
 			return {
-				totalExp: data.total.exp
+				totalExp: data.total.exp,
+				totalInc: data.total.inc,
+				balance: data.balance
 			};
 		},
 
@@ -188,7 +224,7 @@ var budgetController = (function() {
 			if (type === 'exp') {
 				newItem = new Expense(ID, value, comment, category);
 			} else if (type === 'inc') {
-				console.log('not set up yet');
+				newItem = new Income(ID, value, comment, category);
 			}
 
 			data.allItems[type].push(newItem);
@@ -203,12 +239,15 @@ var budgetController = (function() {
 
 })();
 
+////////////////////////////////////////// APP CONTROLLER //////////////////////////////////////////
+
 var controller = (function(budgetCtrl, UICtrl) {
 	
 	var setupEventListners = function() {
 		var DOM, form, category; 
 		DOM = UICtrl.getDOMStrings();
 
+		////////NAV CLICKS
 		var exp = document.getElementById(DOM.navAddExpense)
 	
 		var transactions = document.getElementById(DOM.navAllTransac);
@@ -230,15 +269,20 @@ var controller = (function(budgetCtrl, UICtrl) {
 			function handleIncClick(inc) {
 				UICtrl.getNavBar(DOM.addIncDiv);
 			}
-
+			//////////////////////////////////////////////////
 		//Submit add expense
-		form = document.getElementById(DOM.expBtn);
-		form.addEventListener('click', ctrlAddItem);
-		// form.addEventListener('keypress', function(event) {
-		// 	if (event.keyCode === 13 || event.which === 13) {
-		// 		ctrlAddItem();
-		// 	}
-		// });
+		var addExp = document.getElementById(DOM.expBtn);
+		addExp.onclick = handleAddExpClick;
+		function handleAddExpClick() {
+			ctrlAddItem('exp');
+		}
+		// .addEventListener('click', ctrlAddItem);
+		
+		var addInc = document.getElementById(DOM.incBtn);
+		addInc.onclick = handleAddIncClick;
+		function handleAddIncClick() {
+			ctrlAddItem('inc');
+		}
 	}
 
 	var updateBudget = function() {
@@ -250,20 +294,21 @@ var controller = (function(budgetCtrl, UICtrl) {
 		UICtrl.displayBudget(budget);
 	}
 
-	var ctrlAddItem = function() {
-		var input, newItem;
+	var ctrlAddItem = function(type) {
+		var input, newItem, type;
 
-		input = UICtrl.getInputValue();
-
+		type = type;
+		input = UICtrl.getInputValue(type);
+		
 		if (input.comment !== "" && !isNaN(input.value) && input.value > 0) {
-			newItem = budgetCtrl.addItem('exp', input.value, input.comment, input.category);
+			newItem = budgetCtrl.addItem(type, input.value, input.comment, input.category);
 			console.log(newItem);
 		}
 
 		//Clear input fields
-		UICtrl.clearFields();
+		UICtrl.clearFields(type);
 		//Add item to the UI
-		UICtrl.addListItem(newItem, 'exp');
+		UICtrl.addListItem(newItem, type);
 		//now update budget
 		updateBudget();
 	}
